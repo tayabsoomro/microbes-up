8a6554e513e9041e6fd16b41d23746eb2810f1cb
Erik Tetland <erik.tetland@gmail.com>
2022-09-25 09:24:21 -0600

    chore: splash gif only loops once (old logo - just testing things)

--- src/client/pages/MainMenu.js
+++ src/client/pages/MainMenu.js
@@ -6,7 +6,7 @@ export default class MainMenu extends React.Component {
     render () {
         return (
             <div className="mu-main col-lg-12 border-black">
-                <img src="https://m.tetl.ca/assets/imgs/microbes-up-splash.gif" width="500" />
+                <img src="https://m.tetl.ca/assets/imgs/microbes-up-splash-once.gif" width="500" />
                 <br />           
                 <button onClick={() => window.location.href = 'https://m.tetl.ca/game'} className="btn btn-primary btn-lg" style={{alignSelf: "stretch"}}>Play</button>
             </div>
